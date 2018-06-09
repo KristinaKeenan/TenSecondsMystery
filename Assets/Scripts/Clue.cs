023ff94ea50d6016b97b4244ee455c01ef68b4c1
Kristina Keenan <kkeenan@ithaca.edu>
2018-06-09 15:34:48 -0400

    Working on Board Scripts

---
+++ Assets/Scripts/Clue.cs
@@ -0,0 +1,87 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clue : MonoBehaviour {
+    
+    public string clueName;
+    public bool inInv;
+    public bool isValid;
+    
+    
+    //deals with the board image
+    public GameObject boardImg;
+    public bool imgVisible;
+    
+    
+    //the clues that are connected to this one by string
+    public bool connection1;
+    public bool connection2;
+    
+    public bool inClueList;
+    
+    public bool canInteract;
+    
+    //is it clicked on the board
+    public bool isSelected;
+    
+    //its index in ClueList
+    public int posInClueList;
+    
+	// Use this for initialization
+	void Start () {
+        //initialize variables
+        inInv = false;
+        imgVisible = false;
+        connection1 = false;
+        connection2 = false;
+        inClueList = false;
+		isSelected = false;
+        posInClueList = -1;
+        
+        boardImg.active = true;
+	}
+	
+    public string addToInventory(){
+        inInv = true;
+        return clueName;
+    }
+    
+    public void removeFromInventory(){
+        inInv = false;
+    }
+    
+    public void toggleImageVisibility(){
+        imgVisible = !imgVisible;
+        
+        if(imgVisible){
+            // img code on
+            boardImg.active = true;
+
+        }
+        else{
+            // img code off
+            boardImg.active = false;
+
+        }
+    }
+    
+    public void toggleInteraction(){
+        
+        canInteract = !canInteract;
+        
+        if(canInteract){
+            // toggle on
+        }
+        else{
+            // toggle off
+        }
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Assets/Scripts/ClueBoard.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClueBoard : MonoBehaviour {
+    
+    public List<Clue> clickedClueList;
+    public int validClues;
+    public int currentValidClues;
+    public int lengthCCL;
+
+	// Use this for initialization
+	void Start () {
+        
+        clickedClueList = new List<Clue>();
+        //int validClues = ;
+        currentValidClues = 0;
+        lengthCCL = -1;
+		
+	}
+	
+    public void resetCCL(){
+        clickedClueList = new List<Clue>();
+        currentValidClues = 0;
+        lengthCCL = -1;
+    }
+    
+    public void addToCCL(Clue clickedClue){
+        
+        if(!clickedClue.inClueList && !clickedClue.isSelected){
+            clickedClueList.Add(clickedClue);
+            lengthCCL++;
+            clickedClue.posInClueList = lengthCCL;
+            clickedClue.isSelected = true;
+            
+            if(clickedClue.isValid){
+                currentValidClues++;
+            }
+                
+        }
+        else if(clickedClue.isSelected){
+            //clickedClueList.Remove(clickedClue.posInClueList);
+            lengthCCL--;
+            clickedClue.isSelected = false;
+            
+            if(!clickedClue.isValid){
+                currentValidClues--;
+            }
+
+        }
+    }
+    
+    public void submitBoard(){
+        
+        
+        if(10 == currentValidClues && 10 == (lengthCCL+1)){
+            print("You solved the mystery!");
+        }
+        else if((10==currentValidClues && 10!=(lengthCCL+1)) || ((lengthCCL+1)==currentValidClues && (currentValidClues!=10))){
+            print("You're getting somewhere, but you'll need more evidence.");
+            
+        }
+        else{
+            print("This doesn't make any sense!");           
+        }
+        
+    
+                }
+    
+    
+	// Update is called once per frame
+	void Update () {
+        
+      
+	}
+}
