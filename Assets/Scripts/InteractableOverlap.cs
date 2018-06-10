4ddfa15fc101734e0c3efa0b0d5b122f4eaa7ce8
kbalogh <baloghkaitlyn@gmail.com>
2018-06-10 15:35:50 -0700

    Game now pulls up image when player comes within range of a clue and presses 'space' key.

---
+++ Assets/Scripts/InteractableOverlap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableOverlap : MonoBehaviour {
+   
+    public GameObject interactUI;
+    public GameObject closeup;
+    
+	void OnTriggerEnter(Collider other){
+        
+        interactUI.SetActive(true);
+        
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        interactUI.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown("space")){
+             
+            if (interactUI.activeSelf == true){
+                
+                print("Opening image");
+                closeup.SetActive(true);
+                interactUI.SetActive(false);
+                
+            }
+        }
+	}
+}
