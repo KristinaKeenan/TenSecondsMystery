101668199a0496e88399af36fd8163ebcdd6e705
Kristina Keenan <kkeenan@ithaca.edu>
2018-06-11 20:22:32 -0400

    Changes

---
+++ Assets/Scripts/HelpButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpButton : MonoBehaviour {
+
+    public Button helpButton;
+    
+    public GameObject instructions;
+    
+	// Use this for initialization
+	void Start () {
+        helpButton = GameObject.Find("HelpButton").GetComponent<Button>();
+        helpButton.onClick.AddListener(showInstructions);
+        instructions.SetActive(false);
+
+		
+	}
+	
+    public void showInstructions(){
+        
+                    instructions.SetActive(true);
+        
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        
+         if (Input.GetKeyDown(KeyCode.R)){
+                    instructions.SetActive(false);
+
+            
+        }
+		
+	}
+}
--- Assets/Scripts/SubmitAnswer.cs
+++ Assets/Scripts/SubmitAnswer.cs
@@ -8,25 +8,40 @@ public class SubmitAnswer : MonoBehaviour {
     public Button submitButton;
     
     public ClueBoard clueBoard;
+    
+    public GameObject submitBad, submitGood;
 
 	// Use this for initialization
 	void Start () {
 		submitButton = GameObject.Find("Submit").GetComponent<Button>();
         submitButton.onClick.AddListener(submitAnswer);
         clueBoard = GameObject.Find("clueBoard").GetComponent<ClueBoard>();
+        submitBad = GameObject.Find("SubmitBAD");
+        submitGood = GameObject.Find("SubmitGOOD");
+
+        submitBad.SetActive(false);
+        submitGood.SetActive(false);
+
 	}
 	
     public void submitAnswer(){
         if(7 == clueBoard.currentValidClues && 7 == (clueBoard.lengthCCL+1)){
-            print("You solved the mystery!");
+            submitGood.SetActive(true);
+        
+        
         }
-        else if((7==clueBoard.currentValidClues && 7!=(clueBoard.lengthCCL+1)) || ((clueBoard.lengthCCL+1)==clueBoard.currentValidClues && (clueBoard.currentValidClues!=7))){
+        else{
+            
+            submitBad.SetActive(true);
+            
+        }
+       /* else if((7==clueBoard.currentValidClues && 7!=(clueBoard.lengthCCL+1)) || ((clueBoard.lengthCCL+1)==clueBoard.currentValidClues && (clueBoard.currentValidClues!=7))){
             print("You're getting somewhere, but you'll need more evidence.");
             
         }
         else{
             print("This doesn't make any sense!");           
-        }
+        }*/
         
         
     }
@@ -34,6 +49,15 @@ public class SubmitAnswer : MonoBehaviour {
     
 	// Update is called once per frame
 	void Update () {
+        
+         if (Input.GetKeyDown(KeyCode.R)){
+                    submitBad.SetActive(false);
+
+            
+        }
+        
+        if (Input.GetKey("escape"))
+            Application.Quit();
 		
 	}
 }
